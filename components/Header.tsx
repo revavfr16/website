cfd22781cadf6e9658f52abfa908fd076f7f0b49
David Zager <dzager@redhat.com>
2025-02-25 17:22:40 -0500

    :sparkles: add contact form
    Signed-off-by: David Zager <dzager@redhat.com>

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,37 +13,33 @@ export default function Header() {
   return (
     <header className="relative text-white">
       {isHome ? (
-        <>
-          <div className="container justify-center items-center mx-auto px-4">
-            <Image
-              src={header}
-              alt="Reva Volunteer Fire and Rescue Company 16 Logo"
-              className="w-full h-full object-cover"
-            />
-            <div className="flex justify-center mt-6">
-              <Link
-                href="/donate"
-                className="px-6 py-3 bg-yellow-400 text-black font-bold text-lg rounded-lg shadow-lg hover:bg-yellow-500 transition-colors duration-200"
-              >
-                Donate Now
-              </Link>
-            </div>
-          </div>
-        </>
+        <div className="container justify-center items-center mx-auto px-4">
+          <Image
+            src={header}
+            alt="Reva Volunteer Fire and Rescue Company 16 Logo"
+            className="w-full h-full object-cover"
+          />
+        </div>
       ) : (
         <>
           <div className="container justify-center items-center mx-auto px-4">
-            <Link href="/">
-              <Image
-                src={pathname === "/" ? header : headerNoLineup}
-                alt="Reva Volunteer Fire and Rescue Company 16 Logo"
-                className="w-full h-full object-cover"
-              />
-            </Link>
+            <Image
+              src={pathname === "/" ? header : headerNoLineup}
+              alt="Reva Volunteer Fire and Rescue Company 16 Logo"
+              className="w-full h-full object-cover"
+            />
           </div>
           <div className="top-0 left-0 w-full px-4 py-4 flex justify-center md:relative md:-mt-10">
             <nav>
               <ul className="flex flex-wrap justify-center gap-2 md:gap-4">
+                <li>
+                  <Link
+                    href="/"
+                    className="px-2 py-1 md:px-4 md:py-2 bg-[#a31b1f] text-white rounded hover:bg-white hover:text-[#a31b1f] transition-colors duration-200"
+                  >
+                    Home
+                  </Link>
+                </li>
                 <li>
                   <Link
                     href="/about"
@@ -78,7 +74,7 @@ export default function Header() {
                 </li>
                 <li>
                   <Link
-                    href="/donate"
+                    href="https://www.paypal.com/donate?hosted_button_id=259S9Q9DE54SU"
                     className="px-2 py-1 md:px-4 md:py-2 bg-yellow-400 text-black font-bold rounded hover:bg-yellow-500 transition-colors duration-200"
                   >
                     Donate Now
