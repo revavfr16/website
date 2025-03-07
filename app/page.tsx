35ddca32ad0b9a71ac9132ff76888de95ca7d99d
David Zager <dzager@redhat.com>
2025-03-07 14:10:48 -0500

    :sparkles: add nws alerts
    Signed-off-by: David Zager <dzager@redhat.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
 import Link from "next/link";
+import Image from "next/image";
+import BurnBan from "@/public/BurnBan.jpg";
 
 export default function Home() {
+  const today = new Date();
+  const showBurnBan =
+    (today.getMonth() + 1 === 2 && today.getDate() >= 15) || // February 15th to the end of the month
+    today.getMonth() + 1 === 3 ||
+    today.getMonth() + 1 === 4; // March and April
+
   return (
     <div className="container mx-auto px-4 py-8 space-y-12">
       <section className="grid grid-cols-1 md:grid-cols-4 gap-8">
@@ -48,6 +56,8 @@ export default function Home() {
                     ? "bg-yellow-400 text-black hover:bg-yellow-500"
                     : "bg-red-800 hover:bg-red-700 text-white"
                 } font-bold py-2 px-4 rounded`}
+                target={index === 0 ? "_blank" : undefined}
+                rel={index === 0 ? "noopener noreferrer" : undefined}
               >
                 {item.buttonText}
               </Link>
@@ -56,19 +66,32 @@ export default function Home() {
         ))}
       </section>
 
-      <section className="text-center bg-red-100 py-8 rounded-lg shadow-inner">
-        <h2 className="text-3xl font-semibold mb-4 text-red-800">
-          Upcoming Events
-        </h2>
-        <p className="mb-6 text-gray-700">
-          Join us for our next community gathering!
-        </p>
-        <Link
-          href="/events"
-          className="inline-block bg-red-800 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-        >
-          See All Events
-        </Link>
+      <section className="grid grid-cols-1 gap-8 items-center">
+        {showBurnBan && (
+          <div className="flex justify-center">
+            <Image
+              src={BurnBan}
+              alt="Burn Ban Notice"
+              width={300}
+              height={300}
+              className="rounded-lg shadow-lg"
+            />
+          </div>
+        )}
+        {/* <div className="md:col-span-2 text-center bg-red-100 py-8 rounded-lg shadow-inner">
+          <h2 className="text-3xl font-semibold mb-4 text-red-800">
+            Upcoming Events
+          </h2>
+          <p className="mb-6 text-gray-700">
+            Join us for our next community gathering!
+          </p>
+          <Link
+            href="/events"
+            className="inline-block bg-red-800 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          >
+            See All Events
+          </Link>
+        </div> */}
       </section>
     </div>
   );
